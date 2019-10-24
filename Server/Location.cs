c9ad2a93078b2c638b1879fc4ad36fc77ad3e2d1
Herman Rommetveit <herman.rommetveit@gmail.com>
2019-10-24 12:24:01 +0100

    location object with posibility to add a room at a given location

---
+++ Server/Location.cs
@@ -0,0 +1,34 @@
+﻿using Meeting_Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Location {
+
+        private readonly static String[] locations = new String[5] { "PORTO", "LISBON", "FARO", "COIMBRA",
+            "ÈVORA" };
+
+        private Dictionary<String, List<Room>> rooms = new Dictionary<string, List<Room>>();
+
+        //Adds room to dict based on location if room not in location
+        public void addRoom(Room room, string location) {
+            if (!locations.Contains(location.ToUpper()))
+            {
+                throw new Exception("Can not add a room at this location as location does not exist");
+            }
+            foreach (KeyValuePair<string, List<Room>> entry in rooms) {
+                if (entry.Key == location && !entry.Value.Contains(room))
+                {
+                    entry.Value.Add(room);
+                }               
+            }
+        }
+
+
+
+    }
+}
