0aff6bedfdc443e41c76386b2aafd0fa1fb1bb5b
lurelars <larsmk96@gmail.com>
2019-11-02 16:12:54 +0100

    Developed run script in Client

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
@@ -28,15 +29,54 @@ namespace Client
 
         private void runScript()
         {
-            // open script cile
-            // insert while loop here
-            // line = readline.next()
-            // switch(line):
-            //  case1("createMeeting"):
-            //      this.createMeeting()
-            //      break
-            //  case2("wait"):
-            //      this.wait()
+            string[] command;
+            StreamReader script = new StreamReader(@"PATH");
+            while((command = script.ReadLine().Split(',')) != null)
+            {
+                switch (command[0])
+                {
+                    case "list":
+                        this.listMeetings();
+                        break;
+                    case "create":
+                        this.createMeeting(command[1], Int32.Parse(command[2]),
+                            this.ParseDateLoc(command), this.ListInvitees(command));
+                        break;
+                    case "join":
+                        this.joinMeeting(command[1]);
+                        break;
+                    case "close":
+                        this.closeMeeting(command[1]);
+                        break;
+                    case "wait":
+                        this.wait(Int32.Parse(command[1]));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private List<string> ListInvitees(string[] command)
+        {
+            int m = Int32.Parse(command[4]);
+            List<string> invitees = new List<string>();
+            for (int j = 6; j < 6 + m; j++) {
+                invitees.Add(command[j]);
+            }
+            return invitees;
+        }
+
+        private List<(string, DateTime)> ParseDateLoc(string[] entries)
+        {
+            List<(string, DateTime)> slots = new List<(string, DateTime)>();
+            int n = Int32.Parse(entries[3]);
+            for (int i = 5; i < 5 + n; i++)
+            {
+                string[] dateLoc = entries[i].Replace(@"(", "").Replace(@")", "").Split(';');
+                slots.Add((dateLoc[0], DateTime.Parse(dateLoc[1])));
+            }
+            return slots;
         }
 
         private void wait(int delayTime)
