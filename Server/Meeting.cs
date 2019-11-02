beb695452edc73213eee6778642ea69d57568417
Herman Rommetveit <herman.rommetveit@gmail.com>
2019-11-02 17:16:18 +0000

    finding the the location and date with the most participants and book smallest room

--- Server/Meeting.cs
+++ Server/Meeting.cs
@@ -13,8 +13,8 @@ namespace MeetingCalendar
         private String coordinatorUsername;
         private String topic;
         private int minParticipants;
-        private List<(DateTime, String)> dateLocOptions;
-        private List<String> participants = new List<String>();
+        private List<(string, DateTime)> locDateOptions;
+        private Dictionary<(string, DateTime), List<string>> participants = new Dictionary<(string, DateTime), List<string>>();
         private Boolean closed;
 
         public MeetingServices(String username, String topic, int minParticipants, DateTime date,
@@ -23,21 +23,22 @@ namespace MeetingCalendar
             this.coordinatorUsername = username;
             this.topic = topic;
             this.minParticipants = minParticipants;
-            (DateTime, String) dateLocOption = (date, location);
-            this.dateLocOptions.Add(dateLocOption);
+            (string, DateTime) dateLocOption = (location, date);
+            this.locDateOptions.Add(dateLocOption);
             this.closed = false;
         }
 
         public string Topic { get => topic; }
         public int MinParticipants { get => minParticipants; }
-        public List<(DateTime, string)> DateLocOptions { get => dateLocOptions; }
-        
+        public List<(string, DateTime)> LocDateOptions { get => locDateOptions; }
         public Boolean Closed { set => closed = true;  }
 
-        public void AddParticipant(string part) {
+        public Dictionary<(string, DateTime), List<string>> Participants { get => participants; }
+
+        public void AddParticipantToSlot((string, DateTime) slot, string part) {
             if (!this.closed)
             {
-                this.participants.Add(part);
+                this.participants[slot].Add(part);
             }
         }
 
