4d23ae0743507ad7b1de4af2bedf5969d83bc644
Herman Rommetveit <herman.rommetveit@gmail.com>
2019-11-03 13:08:53 +0000

    imports

--- CommonTypes/CommonType.cs
+++ CommonTypes/CommonType.cs
@@ -12,7 +12,8 @@ namespace CommonTypes
 
         public interface IProcessCreationService
         {
-            void createServer(string serverID, string URL, int max_faults, int min_delay, int max_delay);
+            void createServer(string serverID, string URL, int max_faults, int min_delay, int max_delay, 
+                List<string> otherServerURLs);
             void createClient(string username, string clientURL, string serverURL, string scriptFilePath);
 
         }
--- ProcessCreationService/ProcessCreationService.cs
+++ ProcessCreationService/ProcessCreationService.cs
@@ -37,5 +37,6 @@ namespace ProcessCreationService
             Console.ReadLine();
 
         }
+
     }
 }
